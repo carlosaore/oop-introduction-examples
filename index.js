b46db01d21d9b23e921e59b453a9dff31dfcb918
Pierre van Male <vmalep@protonmail.com>
2021-10-04 16:49:27 +0200

    commit of a first attempt, but there is a mistake in the fonction decl.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello");
+import Vehicle from "./vehicle.js";
+import Car from "./car.js";
+import Motorcycle from "./motorcycle.js";
+
+const myVehicle = new Vehicle("Light blue", "Pescadillac", 500);
+myVehicle.logTypeAndProps();
+
+const myCar = new Car("white", "Papamobile", 4);
+myCar.logTypeAndProps();
+console.log(myCar.lockDoors());
+
+const myMotorcycle = new Motorcycle("black", "BlackMonster", 2);
+myMotorcycle.logTypeAndProps();
+console.log(myMotorcycle.lockDoors());
---
+++ motorcycle.js
@@ -0,0 +1,25 @@
+/*
+1. Write a Vehicle class with:
+    - attributes that represent the color, the name and the number of wheels of the vehicle.
+    - a method "getPropsString" that returns a string with all vehicule properties (ex : "key1 has a value of val1, key2 has a value of val2, ...")
+    - a method "logTypeAndProps" that logs: "Vehicle specs : " followed by the the output of "getPropsString".
+
+2. Then, write the Motorcyle and Car classes that inherit from Vehicle.
+
+3. In the Car class, write a method that only makes sense for cars and not motorcycles (ex: "lockDoors").
+
+4. In Vehicle's derived classes, override the logTypeAndProps method to specify whether the object is an instance of Motorcycle or Car (ex: print "Car specs : ..." or "Motorcycle specs : ..." instead of "Vehicle specs : ...").
+*/
+
+import Vehicle from "./vehicle.js";
+
+class Motorcycle extends Vehicle {
+    constructor(color, name, nbWheels) {
+        super(color, name, nbWheels);
+    }
+    logTypeAndProps() {
+        return console.log("Motorcycle specs: " + this.getPropsString());
+    }
+}
+
+export default class {}
